0e526e52dc5d46d14fbbf7650adc89caa075faa6
GiblerW <westongibler@gmail.com>
2018-11-15 21:49:09 -0700

    headers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import axios from 'axios';
+
 import './App.css';
 
+// const URL = 'http://localhost:3000/recipes';
+
+
 class App extends Component {
+
+  constructor(props) {
+   super(props);
+
+   this.state = {
+     data: [],
+     isLoading: false,
+     error: null,
+   };
+ }
+
+ async componentDidMount() {
+   this.setState({ isLoading: true });
+    const response = await fetch(`https://obscure-woodland-53526.herokuapp.com/recipes`)
+    const json = await response.json()
+     this.setState({data: json})
+    }
+
   render() {
+
+    const { data } = this.state;
+    console.log(this.state)
+    // if (error) {
+    //       return <p>{error.message}</p>;
+    //     }
+    //
+    // if (isLoading) {
+    //   return <p>Loading ...</p>;
+    // }
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Weston
-          </p>
+
+          <ul>
+            {data.map(recipe =>
+              <li key={recipe.id}>
+                <h3>{recipe.title}</h3>
+              </li>
+            )}
+          </ul>
+
         </header>
       </div>
     );
